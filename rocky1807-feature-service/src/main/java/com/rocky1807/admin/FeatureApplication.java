d568fcc7a4f114f0b86b81d1dbe760e37abd9849
Rocky <rocky.xwh@gmail.com>
2023-09-11 01:10:15 +0800

    注册到eureka

--- rocky1807-feature-service/src/main/java/com/rocky1807/admin/FeatureApplication.java
+++ rocky1807-feature-service/src/main/java/com/rocky1807/admin/FeatureApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminApplication {
+public class FeatureApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication.run(FeatureApplication.class, args);
     }
 
 }
