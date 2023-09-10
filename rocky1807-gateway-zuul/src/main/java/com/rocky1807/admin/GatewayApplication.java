0dd0e2cde1c60b84b74375c8ee817949c31a1f7c
Rocky <rocky.xwh@gmail.com>
2023-09-11 02:27:52 +0800

    zuul basic

--- rocky1807-gateway-zuul/src/main/java/com/rocky1807/admin/GatewayApplication.java
+++ rocky1807-gateway-zuul/src/main/java/com/rocky1807/admin/GatewayApplication.java
@@ -2,14 +2,12 @@ package com.rocky1807.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class FeatureApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeatureApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
